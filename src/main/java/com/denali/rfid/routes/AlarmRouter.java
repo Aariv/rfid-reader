e6c5751d473c62adeab6ac36abb06a30f206016a
Ariv <arivbits@gmail.com>
2018-09-11 18:58:05 +0530

    Added pipeline on the Router

--- src/main/java/com/denali/rfid/routes/AlarmRouter.java
+++ src/main/java/com/denali/rfid/routes/AlarmRouter.java
@@ -3,14 +3,9 @@
  */
 package com.denali.rfid.routes;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import com.denali.rfid.dto.AlarmDTO;
-import com.denali.rfid.dto.RfidVisitor;
 
 /**
  * @author zentere
@@ -22,50 +17,49 @@ public class AlarmRouter extends RouteBuilder {
 	@Value("${alarm.route.name}")
 	private String alarmRouterName;
 	
-	@Value("${elastic.bulk.index.host}")
-	private String bulkIndexEndpoint;
-	
-	@Value("${elastic.index.search_alarm_idx}")
-	private String alarmSearchIndexEndpoint;
-	
-	@Value("${mqtt.url}")
-	private String mqttEndpoint;
-	
 	@Override
 	public void configure() throws Exception {
 		
-		//onException(RuntimeException.class).handled(true).log("Error has occured");
+		onException(RuntimeException.class).handled(true).log("Error has occured");
+		
+//		from("direct-vm:processAlarm")
+//			.routeId(alarmRouterName)
+//				.log("Alarm Details -> ${body}")
+//				.setProperty("RFID_ALARM", body())
+//					.process(new Processor() {
+//						
+//						@Override
+//						public void process(Exchange exchange) throws Exception {
+//							RfidVisitor rfidVisitor = (RfidVisitor) exchange.getProperty("RFID_ALARM");
+//							exchange.getIn().setBody(rfidVisitor);
+//						}
+//					})
+//					.to("direct-vm:onReaderMismatch")
+//					.process(new Processor() {
+//						
+//						@Override
+//						public void process(Exchange exchange) throws Exception {
+//							RfidVisitor rfidVisitor = (RfidVisitor) exchange.getProperty("RFID_ALARM");
+//							exchange.getIn().setBody(rfidVisitor);
+//						}
+//					})
+//					.to("direct-vm:onReaderNotFound")
+//					.process(new Processor() {
+//						
+//						@Override
+//						public void process(Exchange exchange) throws Exception {
+//							RfidVisitor rfidVisitor = (RfidVisitor) exchange.getProperty("RFID_ALARM");
+//							exchange.getIn().setBody(rfidVisitor);
+//						}
+//					})
+//					.to("direct-vm:onVisitorExpiredDate");
 		
 		from("direct-vm:processAlarm")
-			.routeId(alarmRouterName)
-				.log("Alarm Details -> ${body}")
-				.setProperty("RFID_ALARM", body())
-					.process(new Processor() {
-						
-						@Override
-						public void process(Exchange exchange) throws Exception {
-							RfidVisitor rfidVisitor = (RfidVisitor) exchange.getProperty("RFID_ALARM");
-							exchange.getIn().setBody(rfidVisitor);
-						}
-					})
-					.to("direct-vm:onReaderMismatch")
-					.process(new Processor() {
-						
-						@Override
-						public void process(Exchange exchange) throws Exception {
-							RfidVisitor rfidVisitor = (RfidVisitor) exchange.getProperty("RFID_ALARM");
-							exchange.getIn().setBody(rfidVisitor);
-						}
-					})
-					.to("direct-vm:onReaderNotFound")
-					.process(new Processor() {
-						
-						@Override
-						public void process(Exchange exchange) throws Exception {
-							RfidVisitor rfidVisitor = (RfidVisitor) exchange.getProperty("RFID_ALARM");
-							exchange.getIn().setBody(rfidVisitor);
-						}
-					})
-					.to("direct-vm:onVisitorExpiredDate");
+		.routeId(alarmRouterName)
+			.log("Alarm Details -> ${body}")
+			.pipeline()
+				.to("direct-vm:onReaderMismatch")
+				.to("direct-vm:onReaderNotFound")
+				.to("direct-vm:onVisitorExpiredDate");
 	}
 }
